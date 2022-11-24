1ed987ee4cad50eb8c34d6256c67e2faf228c5ac
github <git@github.com>
2022-11-24 14:37:45 -0500

    add pandas

--- web-server/store.py
+++ web-server/store.py
@@ -2,9 +2,8 @@ import requests
 
 def get_categories():
     r = requests.get('https://api.escuelajs.co/api/v1/categories')
-    print(r.status_code)
     print(r.text)
     print(type(r.text))
-    categories = r.json()
-    for category in categories:
+    categorias = r.json()
+    for category in categorias:
         print(category['name'])
